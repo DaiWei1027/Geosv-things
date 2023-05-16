d91a43d94d3db14307acffb318d9782234671e01
daiwei <13399818553@163.com>
2023-05-16 08:48:53 +0800

    添加设备常量类

---
+++ things-common/src/main/java/com/things/common/constant/DeviceConstants.java
@@ -0,0 +1,33 @@
+package com.things.common.constant;
+
+/**
+ * @author DaiWei
+ * @date 2023/05/12 16:33
+ **/
+public class DeviceConstants {
+
+    /**
+     * 设备类型 普通设备
+     */
+    public static final String DEVICE = "device";
+
+    /**
+     * 设备类型 网关设备
+     */
+    public static final String GATEWAY = "gateway";
+
+    /**
+     * 协议解析后JSON对象理的 设备编号字段
+     */
+    public static final String DEVICE_NO = "deviceNo";
+
+    /**
+     * 启用
+     */
+    public static final String ENABLE = "0";
+
+    /**
+     * 禁用
+     */
+    public static final String DISABLE = "1";
+}
