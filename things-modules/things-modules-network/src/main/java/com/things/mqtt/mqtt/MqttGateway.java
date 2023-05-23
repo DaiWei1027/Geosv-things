548a9ac130add5c6d9cf036d86e42168c27b7e37
daiwei <13399818553@163.com>
2023-05-23 16:09:19 +0800

    添加发送方法

--- things-modules/things-modules-network/src/main/java/com/things/mqtt/mqtt/MqttGateway.java
+++ things-modules/things-modules-network/src/main/java/com/things/mqtt/mqtt/MqttGateway.java
@@ -1,5 +1,6 @@
 package com.things.mqtt.mqtt;
 
+import com.alibaba.fastjson2.JSONObject;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.mqtt.support.MqttHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -17,8 +18,11 @@ import org.springframework.stereotype.Component;
 public interface MqttGateway {
 
     void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, String data);
+    void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, Object data);
 
     void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, byte[] data);
+
+    void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, JSONObject jsonObject);
 
     void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, @Header(MqttHeaders.QOS) Integer Qos, String data);
 }
